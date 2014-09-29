27df8f8c56413a3f328b38495d550db118373355
Patrick Conant <patrick@prcapps.com>
2014-09-29 10:09:12 -0400

    really adding log directory

--- public_html/index.php
+++ public_html/index.php
@@ -29,8 +29,6 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-
-    <script src="/js/less/less.js" type="text/javascript"></script>
   </head>
 
   <body role="document">
@@ -660,6 +658,6 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="/js/less/bootstrap.min.js"></script>
-    <script src="../../assets/js/docs.min.js"></script>
+
   </body>
 </html>

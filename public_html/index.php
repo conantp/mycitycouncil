04481dd1bd1b7bf8983caaa9f854fcfc64f073fe
Patrick Conant <patrick@prcapps.com>
2014-09-29 10:06:08 -0400

    added readme

--- public_html/index.php
+++ public_html/index.php
@@ -18,6 +18,8 @@
     <!-- Custom styles for this template -->
 
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
 
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
